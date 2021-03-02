00b270fbe0d4ae1d219a9c37c57e8f7d1bcab24b
Verma <AVerma2@INTERNAL.COLT.NET>
2021-03-02 17:27:35 +0530

    adding code

---
+++ Eline.py
@@ -0,0 +1,54 @@
+from netmiko import ConnectHandler
+import time
+import Lib as Lib
+import os
+import yaml
+
+def ELINE(temp, data):
+    print(temp,data)
+
+    # generate template
+    l = Lib.TempG(temp,data)
+
+    # get data from file
+    d1 = Lib.data_file(data)
+
+    # find total devices
+    l = len(d1['data'])
+
+    # get required parameters
+    HOSTNAME=[];LOGIN=[];INTERFACE=[];VLAN=[];N=[]
+    for i in range(l):
+        HOSTNAME.append(d1['data'][i]['HOSTNAME'])
+        LOGIN.append(d1['data'][i]['LOGIN'])
+        INTERFACE.append(d1['data'][i]['interface'])
+        VLAN.append(d1['data'][i]['vlan'])
+        N.append(d1['data'][i]['n'])    # total number of service to be created
+    print(HOSTNAME, LOGIN, INTERFACE, VLAN, N)
+
+    # netmiko ssh connection//ssh to all devices
+    net_ssh=[]
+    for i in range(l):
+        net_ssh.append(Lib.net_con(HOSTNAME[i], LOGIN[i]))
+    
+    print(net_ssh)
+
+    # verification/show command
+    for i in range(l):
+        try:
+            for j in range(d1['data'][i]['n']):                
+                output1= net_ssh[i].find_prompt()
+                command= 'show l2vpn xconnect interface {}.{}'.format(INTERFACE[i],str((int(VLAN[i]+j))))
+                output2= net_ssh[i].send_command_expect(command)
+                log = '{}{}\n{}\n\n'.format(output1,command,output2)
+                Lib.deviceLog(log)
+                print(log)           
+            print(net_ssh[i].disconnect())
+            
+        except Exception as e:
+                log = '\n {} \n'.format(str(e))
+                Lib.deviceLog(log)
+                print(log)
+
+ELINE('eline.jinja','ar1-ar6-115.yml')
+# ELINE('eline_del.jinja','ar1-ar6-115.yml')
---
+++ Lib.py
@@ -0,0 +1,110 @@
+from netmiko import ConnectHandler
+import time, re
+from time import gmtime, strftime
+from jinja2 import Template
+import json
+import yaml
+import argparse
+import os
+
+
+
+def configLog(config,Hostname):
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    print(file_path)
+    f = open(file_path + '\\Config\\' + Hostname + '.cfg', 'w+')   # old config will be removed. This config file will be pushed to device.
+    f.write(config)
+    f.write('\n\n')
+    time.sleep(0.5)
+    f.close()
+
+
+def deviceLog(log):
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    print(file_path)
+    LocalTime= strftime("%d%m%Y", gmtime())
+    f = open(file_path + '\\Log\\DeviceLog-' + LocalTime + '.txt', 'a')    # append log in the same file
+    f.write(log)
+    time.sleep(0.5)
+    f.close()
+
+
+def data_file(data):
+    # automatic path for data file
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    for i,j,k in os.walk(file_path):
+        if data in k:
+            d1 = i + '\\' + data
+    # d1 = 'Template/'+d
+    d2 = yaml.load(open(d1), Loader=yaml.FullLoader)   # Loader=yaml.Loader --> on Jump Server
+    return d2
+
+
+def TempG(temp, data):
+    # automatic path for template file
+    file_path = os.path.dirname(os.path.realpath(__file__))
+    for i,j,k in os.walk(file_path):
+        if temp in k:
+            t = i + '\\' + temp
+    # t = 'Template/' + temp
+    f = open(t,'r')
+    tem = f.read()
+    #print ('temp')  # to verify template#
+
+    d1 = {}  # define dic
+    d1 = data_file(data)   # get json/yaml format data in d1. (class dict is returned)
+    # find the no of variables in data file
+    #print(d1)
+    l = len(d1['data'])
+    print('\nTotal devices = ' + str(l) + '\n\n')  # to verify number of inputs in data file (device to be configured)
+    #print (d1['data'])
+
+    for i in range(l):
+        try:
+            template = Template(tem)
+            config = template.render(component=d1['data'][i])
+
+            Hostname = d1['data'][i]['HOSTNAME']   # create config file with same name
+
+            print ('\n\n' + Hostname + " Config Prepared as below -\n")
+            print (config)
+            # return config, Hostname
+            configLog(config,Hostname)  # config & Hostname name is passed to create log file#
+            time.sleep(0.5)
+
+        except Exception as e:
+            log = '\n {} \n'.format(str(e))
+            deviceLog(log)
+            print(log)
+
+
+def ConfigP(Hostname, dip):
+
+    print ('\nDevice to be configured= ' + dip + '\n')
+    # push config on device and create log
+    net_connect = ConnectHandler(device_type=deviceType, ip=dip, username=username, password=pw, port=port)
+    #output1 = net_connect.send_config_set(config)
+    config_file = 'Config/' + Hostname + '.cfg'
+    output1 = net_connect.send_config_from_file(config_file)
+    time.sleep(5)
+    output2 = net_connect.commit()
+    time.sleep(4)
+    output3 = net_connect.exit_config_mode()
+    output4 = net_connect.disconnect()
+
+    c = ('\n----'+dip+'----\n'+'\n'+output1+output2+output3+'\n'+'\n---- END ----\n')
+    print (c)
+    deviceLog(c)
+    time.sleep(1)
+
+def net_con(HOSTNAME,login_details):
+    try:
+        net_ssh = ConnectHandler(**login_details)
+        log = '\n**netmiko ssh connection {}\n'.format(HOSTNAME)
+        print(log)
+        deviceLog(log)
+        return(net_ssh)
+    except Exception as e:
+        log = '\n**netmiko connection failed {}\n'.format(e)
+        print(log)
+        deviceLog(log)
